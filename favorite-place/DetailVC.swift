b6a3527f3a716f1be5dbacca47d5b7ab4c56114f
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-07 22:38:46 +0000

    created an app about YUCCA VALLAEY, CA

---
+++ favorite-place/DetailVC.swift
@@ -0,0 +1,38 @@
+//
+//  DetailVC.swift
+//  favorite-place
+//
+//  Created by Vyacheslav Horbach on 07/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- favorite-place/ViewController.swift
+++ favorite-place/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moreBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        moreBtn.layer.cornerRadius = 5.0
+        
     }
 
     override func didReceiveMemoryWarning() {
